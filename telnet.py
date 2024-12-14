c6e35b1092702b0c609c78c02494d0ecf7a9fc68
Eashaan <120644754+Ish56-wq@users.noreply.github.com>
2024-12-14 14:44:18 +0530

    Add telnet.py

--- telnet.py
+++ telnet.py
@@ -25,6 +25,41 @@
 
 import socket
 import ssl
+import tkinter
+
+
+window = tkinter.Tk()
+tkinter.mainloop()
+
+WIDTH, HEIGHT = 800, 600
+
+class Browser:
+    def _init_(self):
+        self.window = tkinter.Tk()
+        self.canvas = tkinter.Canvas(
+            self.window,
+            width=WIDTH,
+            height=HEIGHT
+        )
+        self.canvas.pack()
+        
+
+    def load(self, url):
+        body = url.request()
+        text = lex(body)
+        self.canvas.create_rectangle(10, 20, 400, 300)
+        self.canvas.create_oval(100, 100, 150, 150)
+        self.canvas.create_text(200, 150, text="Hi")
+
+        HSTEP, VSTEP = 13, 18
+        cursor_x, cursor_y = HSTEP, VSTEP
+        for c in text:
+            self.canvas.create_text(cursor_x, cursor_y, text=c)
+            cursor_x += HSTEP
+
+            if cursor_x >= WIDTH - HSTEP:
+                cursor_y += VSTEP
+                cursor_x = HSTEP
 
 class URL:
     def _init_(self, url):
@@ -69,22 +104,25 @@ class URL:
         s.close()
         return content
 
-def show(body):
+def lex(body):
+    text = ""
     in_tag = False
+
     for c in body:
         if c == "<":
             in_tag = True
         elif c == ">":
             in_tag = False
         elif not in_tag:
-            print(c, end="")
+            text += c
+    return text    
 
-def load(url):
-    body = url.request()
-    show(body)
+
+
 
 if _name_ == "_main_":
     import sys
-    load(URL(sys.argv[1]))
+    Browser().load(URL(sys.argv[1]))
+    tkinter.mainloop()
 
 #python pathto/telnet.py http://example.org/
